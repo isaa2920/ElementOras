2be0e8379cb99bd43f263ee6dc7c582ca106292d
isaa2920 <isaa2920@scdsb.on.ca>
2025-10-09 08:12:10 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+
+namespace ElementOras;
+
+internal static class Program
+{
+    public const int WindowSizeX = 800;
+    public const int WindowSizeY = 480;
+    public const string ApplicationName = "ElementOras";
+
+    [System.STAThread]
+    public static void Main()
+    {
+        Raylib.InitWindow(WindowSizeX, WindowSizeY, ApplicationName);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
